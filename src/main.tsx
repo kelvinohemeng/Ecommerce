50e95f527a110ab7c55c21f23ae7814264a9c0ac
kelvin Ohemeng <Kelvin@RealtyView.com>
2025-02-06 17:38:21 +0000

    prep

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import "./index.css";
+import App from "./App.tsx";
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className=" bg-red-300 text-bold">Home</div>;
+};
+
+export default Home;
